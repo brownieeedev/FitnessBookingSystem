3ea6d42d58d769eda3cfd1f51a47bf3caa4bc245
Barna Csordas <brownieeedev@gmail.com>
2023-12-05 09:49:28 +0100

    add base of App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+//Packages
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 //Components
-
+import Nav from "./components/nav/Nav";
 //Pages
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Nav />
+      <Nav
+        tabs={[
+          { name: "Home", navigateTo: "/" },
+          { name: "Trainings", navigateTo: "/trainings" },
+          { name: "Trainers", navigateTo: "/trainers" },
+          { name: "Videos", navigateTo: "/videos" },
+        ]}
+      />
       <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}/>>
-        </Routes>
+        <div className="max-w-[1250px] m-auto">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
